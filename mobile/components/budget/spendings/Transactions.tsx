400d9c469cd67f3b1dbac85559aada478b3742ac
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2026-05-21 21:14:14 +0530

    Spending list created

--- mobile/components/budget/spendings/Transactions.tsx
+++ mobile/components/budget/spendings/Transactions.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { View } from "react-native";
+import { View,Text } from "react-native";
 import { transactions } from "@/styles/global";
 
 export default function Transactions({data}:{data:{id:string, title:string, amount:number}}){
     
     return(
         <View style={transactions.container}>
-            
+            <View>
+                <Text>{data.title}</Text>
+            </View>
+            <Text>₹{data.amount.toFixed(2)}</Text>
         </View>
     )
 }
